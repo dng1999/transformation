64c97c74ad8e929022290ecfbda1b8f0f1d7de1b
Dorothy Ng <dng1@stuy.edu>
2017-03-05 23:34:46 -0500

    TypeError unresolved

--- matrix.py
+++ matrix.py
@@ -5,6 +5,7 @@ def make_translate( x, y, z ):
     m[0][3] = x
     m[1][3] = y
     m[2][3] = z
+    m[3][3] = 1
     return m
     
 def make_scale( x, y, z ):
@@ -12,18 +13,36 @@ def make_scale( x, y, z ):
     m[0][0] = x
     m[1][1] = y
     m[2][2] = z
+    m[3][3] = 1
     return m
 
 def make_rotX( theta ):    
     m = ident(new_matrix())
-    return m
+    rad = math.radians( theta )
+    m[1][1] = math.cos( rad ) 
+    m[1][2] = -1 * math.sin( rad )
+    m[2][1] = math.sin( rad )
+    m[2][2] = math.cos( rad )
+    m[3][3] = 1    
+    return m;
     
 def make_rotY( theta ):
     m = ident(new_matrix())
-    return m
+    rad = math.radians( theta )
+    m[0][0] = math.cos( rad ) 
+    m[3][0] = -1 * math.sin( rad )
+    m[0][3] = math.sin( rad )
+    m[3][3] = math.cos( rad )
+    return m;
 
 def make_rotZ( theta ):
     m = ident(new_matrix())
+    rad = math.radians( theta )
+    m[0][0] = math.cos( rad ) 
+    m[0][1] = -1 * math.sin( rad )
+    m[1][0] = math.sin( rad )
+    m[1][1] = math.cos( rad )
+    m[3][3] = 1
     return m
     
 def print_matrix( matrix ):
--- parser.py
+++ parser.py
@@ -38,4 +38,59 @@ The file follows the following format:
 See the file script for an example of the file format
 """
 def parse_file( fname, points, transform, screen, color ):
-    pass
+    file = open(fname,'r')
+    line = file.readline().strip()
+    while (line != "quit" and line != ""):
+        cmd = line
+        if cmd == "line":
+            args = file.readline().strip().split(" ")
+            if len(args) < 6:
+                print("not enough args")
+            else:
+                add_edge(points, int(args[0]), int(args[1]), int(args[2]), int(args[3]), int(args[4]), int(args[5]))
+        elif cmd == "ident":
+            ident(transform)
+        elif cmd == "scale":
+            args = file.readline().strip().split(" ")
+            if len(args)< 3: 
+                print("not enough args")
+            else:
+                matrix_mult(make_scale(int(args[0]), int(args[1]), int(args[2])),transform)
+        elif cmd == "rotate":
+            args = file.readline().strip().split(" ")
+            if len(args) < 2:
+                print("not enough args")
+            else:
+                if (args[0] == 'x'):
+                    matrix_mult(make_rotX(int(args[1])), transform)
+                elif (args[0] == 'y'):
+                    matrix_mult(make_rotY(int(args[1])), transform)
+                else:
+                    matrix_mult(make_rotZ(int(args[1])), transform)
+        elif cmd == "yrotate":
+            args = file.readline().strip().split(" ")
+            if len(args) < 1:
+                print("not enough args")
+            else:
+                matrix_mult(make_rotY(int(args[0])), transform)
+        elif cmd == "zrotate":
+            args = file.readline().strip().split(" ")
+            if len(args) < 1:
+                print("not enough args")
+            else:
+                matrix_mult(make_rotZ(int(args[0])), transform)
+        elif cmd == "apply":
+            matrix_mult(transform, points)
+        elif cmd == "display":
+            draw_lines(points, screen, color)
+            display(screen)
+        elif cmd == "save":
+            args = file.readline().strip().split(" ")
+            if len(args) < 1:
+                print("not enough args")
+            else:
+                draw_lines(points, screen, color)
+                save_extension(screen, args[0])
+        else:
+            print("no command")
+        line = file.readline().strip()
